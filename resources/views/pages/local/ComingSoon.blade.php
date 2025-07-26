16590b01436238fb53fd6668c563754152321bfe
IDCDEV03 <idcdev03@iddrives.co.th>
2025-07-27 00:00:25 +0700

    update-contact
    26-07-68 แก้ไขหน้า comingsoon

--- resources/views/pages/local/ComingSoon.blade.php
+++ resources/views/pages/local/ComingSoon.blade.php
@@ -8,9 +8,13 @@
                <div class="col-12">
                   <div class="min-vh-100 content-center">
                      <div class="maintenance-page text-center">
-                        <img src="{{asset('coming_soon.png')}}" alt="maintenance" width="256px" />
-                        <h5 class="maintenance-page__title">We are currently performing maintenance</h5>
-                        <p class="fw-500">We're making the system more awesome.We'll be back shortly.</p>
+                        <img src="{{asset('chat.png')}}" alt="maintenance" width="256px" />
+                        <br>
+                        <span class="fs-24 fw-bold maintenance-page__title">สนใจใช้งานระบบติดต่อได้ที่</span>
+                        <br>
+                        <span class="fs-20 fw-500">
+                          <a href="mailto:idc@iddrives.co.th">idc@iddrives.co.th</a>
+                           หรือ โทร.065-083-7000</span>
                      </div>
                   </div>
                </div>
